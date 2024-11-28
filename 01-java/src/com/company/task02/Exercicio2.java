246173db852914087239e1ea4a9d7f744773fb38
Arley Castro <arleycodeqa@gmail.com>
2024-11-28 08:54:58 -0400

    Adicionar configuração inicial do exercício 2 e menu

--- 01-java/src/com/company/task02/Exercicio2.java
+++ 01-java/src/com/company/task02/Exercicio2.java
@@ -1,7 +1,28 @@
 package com.company.task02;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] args) {
+        int jogosTotal = 100;
+        String[] timesCasa = new String[jogosTotal];
+        String[] timesVisitantes = new String[jogosTotal];
+        int[] pontuacoesCasa = new int[jogosTotal];
+        int[] pontuacoesVisitantes = new int[jogosTotal];
+        String[] datasJogos = new String[jogosTotal];
 
+        int totalJogos = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nMenu de Opções:");
+            System.out.println("1. Inserir novo jogo");
+            System.out.println("2. Exibir todos os jogos");
+            System.out.println("3. Buscar jogos por time");
+            System.out.println("4. Exibir classificação dos times");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
     }
 }
