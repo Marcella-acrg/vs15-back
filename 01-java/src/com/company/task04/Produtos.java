3fd5e639e99a6393793965c011caacb874ff7e58
Marcella <marcellaacoelho@gmail.com>
2024-12-02 06:14:18 -0300

    classe produtos

---
+++ 01-java/src/com/company/task04/Produtos.java
@@ -0,0 +1,31 @@
+package com.company.task04;
+
+public class Produtos {
+
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Produtos(String nome, double preco, int quantidadeEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+}
+
